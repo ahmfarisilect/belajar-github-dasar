304c8bc0b3b4476908934d9b6ec9b5f50ecd38fd
ahmfarisilect <ahmadfarisi@mdp.ac.id>
2022-09-26 00:17:45 +0700

    Membuat File 2017-C1.cpp
    > Menyelesaikan permasalahan yang ada pada Soal Olimpiade Informatika tahun 2017, bagian C1

---
+++ 2017-C1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int min(int x, int y){
+	if(x > y){
+		return y;
+	}
+	else{
+		return x;
+	}
+}
+
+//pembulatan ke atas (ceil division)
+int cd(int x, int y){
+	return (x+y-1) / y;
+}
+
+int main(){
+	int n,m,x,y;
+	
+	cin>>n>>m>>x>>y;
+	
+	if(x>cd(n, 2)){
+		x = n - x + 1;
+	}
+	
+	if(y>cd(m, 2)){
+		y = m - y + 1;
+	}
+	
+	cout<<min(x,y);
+
+//	cout<<min(min(x, n-x+1), min(y, m-y+1));
+	
+	getch();
+	
+	return 0;
+}
